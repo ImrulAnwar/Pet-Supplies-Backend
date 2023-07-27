4f16d700d8854581a21a81407f87f63648a2eeaf
Imrul Anwar <imrulanwar836@gmail.com>
2023-07-27 07:23:37 +0600

    fixed the urls

--- item_api/urls.py
+++ item_api/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from .views import ItemView, SingleItemView
 
 urlpatterns = [
-     path('all/', ItemView.as_view(), name = 'all_items'),
-     path('<slug:slug>', SingleItemView.as_view(), name = 'single_item')
+    path('all', ItemView.as_view(), name='all_items'),
+    path('<slug:slug>', SingleItemView.as_view(), name='single_item')
 ]
