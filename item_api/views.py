d09a5a182d1a4fefa80ff3d2e80eedc8c8a55ae0
Imrul Anwar <imrulanwar836@gmail.com>
2023-07-27 07:19:26 +0600

    deleting an item will delete the image as well

--- item_api/views.py
+++ item_api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.views import APIView
-from .models import Item 
+from .models import Item
 from .serializers import ItemSerializer
 from rest_framework.response import Response
 from rest_framework import status, permissions
+import os
+
 
 class IsSuperUserOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
@@ -10,51 +12,60 @@ class IsSuperUserOrReadOnly(permissions.BasePermission):
             return True
         return request.user and request.user.is_superuser
 
+
 class ItemView(APIView):
     permission_classes = [IsSuperUserOrReadOnly]
+
     def get(self, request):
-        try: 
+        try:
             items = Item.objects.all()
-            serializer = ItemSerializer(items, many = True)
+            serializer = ItemSerializer(items, many=True)
             return Response(serializer.data)
-        except Exception as e: 
-            return Response(str(e), status= status.HTTP_400_BAD_REQUEST)
-        
+        except Exception as e:
+            return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+
     def post(self, request):
-        try: 
-            serializer = ItemSerializer(data = request.data)
+        try:
+            serializer = ItemSerializer(data=request.data)
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except Exception as e: 
+        except Exception as e:
             return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
-        
+
+
 class SingleItemView(APIView):
     permission_classes = [IsSuperUserOrReadOnly]
+
     def get(self, request, slug):
-        try: 
-            item = Item.objects.get(slug = slug)
+        try:
+            item = Item.objects.get(slug=slug)
             serializer = ItemSerializer(item)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Item.DoesNotExist:
-            return Response(status = status.HTTP_404_NOT_FOUND)
-        
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
     def put(self, request, slug):
         try:
-            item = Item.objects.get(slug = slug)
-            serializer = ItemSerializer(item, data = request.data)
+            item = Item.objects.get(slug=slug)
+            serializer = ItemSerializer(item, data=request.data)
             if serializer.is_valid():
                 serializer.save()
-                return Response(serializer.data, status = status.HTTP_200_OK)
-            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Item.DoesNotExist:
-            return Response(status = status.HTTP_404_NOT_FOUND)
-        
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
     def delete(self, request, slug):
         try:
-            item = Item.objects.get(slug = slug)
+            item = Item.objects.get(slug=slug)
+            # Check if the product_image field contains a file
+            if item.product_image:
+                file_path = item.product_image.path
+                if os.path.exists(file_path):
+                    os.remove(file_path)
             item.delete()
-            return Response("Item Deleted", status = status.HTTP_204_NO_CONTENT)
+            return Response("Item Deleted", status=status.HTTP_204_NO_CONTENT)
         except Item.DoesNotExist:
             return Response(status.HTTP_404_NOT_FOUND)
